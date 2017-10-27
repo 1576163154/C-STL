cfe5c17e5a3d102c62d32b079ad073fa0ec95d1f
xxjiang <849483053@qq.com>
2017-10-27 18:22:18 +0800

    C strchr 寻找以某字符串开始的子串

---
+++ priority queue/strchr/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+
+void main() {
+	const char *str = "Try not. Do, or do not. There is no try.";
+  char target = 'T';
+  const char *result = str;
+ 
+  while((result = strchr(result, target)) != NULL) {
+	  cout << result << endl;
+    ++result; // Increment result, otherwise we'll find target at the same location
+  }
+	system("pause");
+}
